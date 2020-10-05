89d40345c45f0c163d70700ff260974d432669cc
Kishor Naik <kishor.naik011.net@outlook.com>
2020-10-05 19:38:57 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [StringLength(50)]
+        public String FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public String EmailId { get; set; }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Pages/Demo/UserComponent.razor.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Sol_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Pages.Demo
+{
+    public partial class UserComponent
+    {
+        #region Constructor
+
+        public UserComponent()
+        {
+            UserM = new UserModel();
+        }
+
+        #endregion Constructor
+
+        #region Private Property
+
+        private UserModel UserM { get; set; }
+
+        private bool IsDisplay { get; set; }
+
+        #endregion Private Property
+
+        #region Protected Event Handler
+
+        protected void OnSubmit(EditContext formContext)
+        {
+            var flag = formContext?.Validate();
+            if (flag == false) return;
+
+            // Do Someting
+
+            // Demo Code
+            IsDisplay = true;
+        }
+
+        protected void OnDelete(EditContext formContext)
+        {
+            var flag = formContext?.Validate();
+            if (flag == false) return;
+
+            // Do Something
+
+            // Demo Code
+            IsDisplay = true;
+        }
+
+        protected void OnCancel()
+        {
+            this.UserM.EmailId = null;
+            this.UserM.FullName = null;
+
+            IsDisplay = false;
+
+            // Do Something
+        }
+
+        #endregion Protected Event Handler
+    }
+}
